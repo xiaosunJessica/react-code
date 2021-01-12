6b409949c2972f1dfbd37533342fdbcfc6d87250
sunyaqin001 <sunyaqin001@ke.com>
2021-01-12 19:01:01 +0800

    other

--- ReactV2-Packages/src/store/index.js
+++ ReactV2-Packages/src/store/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import ReactDOM from './react/packages/react-dom';
-import App from './page/App';
-// import App from './page/AppHooks';
 import thunkMiddleware from "redux-thunk";
+import {createLogger} from "redux-logger";
+import { combineReducers } from "redux";
+import counter from './reducer';
 import {createStore, applyMiddleware, compose} from 'redux'
 // import * as serviceWorker from './serviceWorker';
-import store from './store';
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+const composeEnhancers = compose;
+const devtools = window.devToolsExtension || (() => (noop) => noop);
+const middleWares = [thunkMiddleware, createLogger()]
+const store = createStore(combineReducers({counter}), composeEnhancers([applyMiddleware(...middleWares), devtools]));
 
+export default store
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
